c5a675f414cd5ca5d119656d50336675c6942cfe
Asgar Rashid <asgar.rashid019@gmail.com>
2026-08-01 16:54:07 +0530

    Prune repository: keep UG project files for thesis

--- frontend/src/pages/HomePage.jsx
+++
@@ -1,9 +0,0 @@
-import { HeroSection } from "../components/sections/HeroSection"
-
-export function HomePage() {
-  return (
-    <>
-      <HeroSection />
-    </>
-  )
-}
--- utils/file_utils.py
+++
@@ -1,9 +0,0 @@
-import os
-from werkzeug.utils import secure_filename
-from config import UPLOAD_FOLDER
-
-def save_uploaded_file(file):
-    filename = secure_filename(file.filename)
-    filepath = os.path.join(UPLOAD_FOLDER, filename)
-    file.save(filepath)
-    return filename, filepath
